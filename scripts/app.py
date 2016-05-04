8555f6c4076a485d7615b8caef861536096c0ac1
George Theofilis <theofilis.g@github.com>
2016-05-04 22:48:56 +0300

    Load source dataset and save transformed dataset

--- scripts/app.py
+++ scripts/app.py
@@ -2,7 +2,6 @@ from rsk_mind.datasource import CSVDatasource
 
 datasource = CSVDatasource('in.csv')
 dataset = datasource.read()
-dataset.setTransformer(1)
 dataset.applyTransformations()
 datasource = CSVDatasource('out.csv')
 datasource.write(dataset)
