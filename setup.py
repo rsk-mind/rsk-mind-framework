55683d53e9be7972cc3d5500ba72fa146184cf51
George Theofilis <theofilis.g@gmail.com>
2016-05-05 19:15:27 +0300

    Solve some issues on setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 __version__ = "0.1.1"
 
 setup(
-    name='rsk_mind',
+    name='RSK Mind',
     version=__version__,
     description='Framework for machine learning platform',
     keywords='machine learning deep learning',
-    url='git@bitbucket.org:rasarmy/fraud-backend.git',
-    author='RSK Mind',
-    author_email='',
+    url='git@bitbucket.org:rasarmy/framework.git',
+    author='RSK Project',
+    author_email='admin@rsk-project.com',
     license='MIT',
-    packages=['rsk_mind', 'rsk_mind.engine', 'rsk_mind.datasource',
-              'rsk_mind.classifier', 'rsk_mind.transformer',
-              'rsk_mind.filter', 'rsk_mind.dataset'],
-    # dependent packages
+    scripts=['rsk_mind/bin/rskmind-admin.py'],
+    entry_points={'console_scripts': [
+        'rskmind-admin = rskmind.core.management:execute_from_command_line',
+    ]},
+    include_package_data=True,
+    packages=find_packages(exclude=('tests', 'tests.*')),
     install_requires=[
             'numpy',
             'scipy',
             'xgboost==0.4a30',
             'geoip2',
-            'scikit-learn'
+            'scikit-learn',
+            'Jinja2'
     ],
     extras_require={
             'docs': ['sphinx']
     },
-    zip_safe=False)
+    zip_safe=False
+)
