d7c3619e8e8952d4eed02c1b26b9f11a4891043c
George Theofilis <theofilis.g@gmail.com>
2016-05-05 19:15:10 +0300

    Add script for framework

--- rsk_mind/bin/rskmind-admin.py
+++ rsk_mind/bin/rskmind-admin.py
@@ -1,20 +1,7 @@
+#!/usr/bin/env python
+from rsk_mind.core import management
+
 import sys
 
-import argparse
-
-def main(argv):
-
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument(
-        'command', help='')
-    parser.add_argument(
-        'project_name', help='')
-    # project_name/transformer.py
-    # project_name/setting.py
-    # project_name/manager.py
-    params = parser.parse_args(argv)
-
-    print params
-
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   management.execute_from_command_line(sys.argv)
