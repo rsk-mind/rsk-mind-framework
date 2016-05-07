5838e9dbd7affed229645986b93428ace5a158b1
George Theofilis <theofilis.g@gmail.com>
2016-05-07 23:41:37 +0300

    Simple transformation engine

--- rsk_mind/dataset/dataset.py
+++ rsk_mind/dataset/dataset.py
@@ -6,18 +6,15 @@ class Dataset():
         self.header = header
         self.rows = rows
         self.transformer = None
-        self.trn = {}
         self.transformed_rows = []
-        self.transformed_header = []
+        self.transformed_header = None
 
     def setTransformer(self, transformer):
         """ """
         self.transformer = transformer
-        self.trn = self.transformer.__dict__
 
     def applyTransformations(self):
         """ """
-        self.transformed_header = self.header
         for row in self.rows:
             self.applyRowTransformation(row)
 
@@ -25,15 +22,30 @@ class Dataset():
         """ """
         if self.transformer == None:
             transformed_row = row
+            transformed_header = self.header
         else:
             transformed_row = []
+            transformed_header = []
             for i, cell in enumerate(row):
                 feat = self.header[i]
-                if feat in self.trn:
-                    mytrans = self.trn[feat]
-                else:
+                try:
+                    mytrans = getattr(self.transformer, feat)
+                except:
                     mytrans = self.transformer.default
 
-                transformed_row += mytrans(cell)
+                transformed_feat = mytrans(cell)
 
+                if self.transformed_header is None:
+                    if len(transformed_feat) == 0:
+                        pass
+                    elif len(transformed_feat) == 1:
+                        transformed_header.append(feat)
+                    else:
+                        for j in range(len(transformed_feat)):
+                            transformed_header.append("%s_%s" % (feat, j))
+
+                transformed_row += transformed_feat
+
+        if self.transformed_header is None:
+            self.transformed_header = transformed_header
         self.transformed_rows += [transformed_row]
