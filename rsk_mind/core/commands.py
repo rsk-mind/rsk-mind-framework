7e2124f66eca85a3f341f6878467f8aa7965cbd3
George Theofilis <theofilis.g@gmail.com>
2016-05-07 19:50:28 +0300

    Add transformation command

--- rsk_mind/core/commands.py
+++ rsk_mind/core/commands.py
@@ -1,3 +1,6 @@
+import argparse, os
+
+
 def transformation(setting):
     DATASOURCE = setting.DATASOURCE
 
@@ -6,3 +9,16 @@ def transformation(setting):
 
     datasource = DATASOURCE['OUT']['class'](*DATASOURCE['OUT']['params'])
     datasource.write(dataset)
+
+
+def execute_from_command_line(argv, setting):
+    argv = argv[1:]
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('command', help='Command to execute!')
+    params = parser.parse_args(argv)
+
+    if params.command == 'transformation':
+        transformation(setting)
+    else:
+        pass
