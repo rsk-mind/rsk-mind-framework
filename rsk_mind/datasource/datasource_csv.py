179c13d3fe2589d43e260da86e0465901d149a80
George Theofilis <theofilis.g@gmail.com>
2016-05-09 00:16:33 +0300

    Set targe class on csv document

--- rsk_mind/datasource/datasource_csv.py
+++ rsk_mind/datasource/datasource_csv.py
@@ -4,6 +4,10 @@ from ..dataset import Dataset
 
 class CSVDatasource(Datasource):
 
+    def __init__(self, path, target=None):
+        super(CSVDatasource, self).__init__(path)
+        self.target = target
+
     def read(self):
         with open(self.path, 'rb') as infile:
             reader = csv.reader(infile)
@@ -12,6 +16,11 @@ class CSVDatasource(Datasource):
 
             rows = []
             for row in reader:
+                if self.target is not None:
+                    index = header.index(self.target)
+                    target = row[index]
+                    del row[index]
+                    row += [target]
                 rows.append(row)
 
         return Dataset(header, rows)
@@ -21,6 +30,6 @@ class CSVDatasource(Datasource):
         with open(self.path, 'w') as outfile:
             writer = csv.writer(outfile)
             writer.writerow(dataset.transformed_header)
-            
+
             for row in dataset.transformed_rows:
                 writer.writerow(row)
