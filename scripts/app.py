beac454ebf93e336cdcc86aca25adc4b4685a11f
George Theofilis <theofilis.g@gmail.com>
2016-05-08 16:21:04 +0300

    Remove script demo code

--- scripts/app.py
+++
@@ -1,9 +0,0 @@
-from rsk_mind.datasource import CSVDatasource
-
-from .settings import *
-
-datasource = CSVDatasource('in.csv')
-dataset = datasource.read()
-dataset.applyTransformations()
-datasource = CSVDatasource('out.csv')
-datasource.write(dataset)
