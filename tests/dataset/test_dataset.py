857583dc9177e08df5db37ef249887ecf461bc2c
George Theofilis <theofilis.g@gmail.com>
2016-05-12 16:27:29 +0300

    Test all methods on dataset

--- tests/dataset/test_dataset.py
+++ tests/dataset/test_dataset.py
@@ -4,11 +4,28 @@ from nose.tools import assert_equals, assert_items_equal
 from rsk_mind.dataset import Dataset
 from rsk_mind.transformer import *
 
+class CustomTransformer(Transformer):
+    class Feats():
+        a1 = Feat()
+        a2 = Feat()
+
+        f1 = CompositeFeat(['a1', 'a2'])
+
+    def get_a1(self, feat):
+        return [-float(feat)]
+
+    def get_a2(self, feat):
+        return [-float(feat)]
+
+    def get_f1(self, a1, a2):
+        return [float(a1) + float(a2)]
+
+
 class TestDataset:
 
     def setUp(self):
         self.header = ['a1', 'a2', 'y']
-        self.rows = [['0', '0', '1'], ['0', '2', '1'], ['1', '0.5', '1'] ,['0.9', '2', '1']]
+        self.rows = [['0', '0', '0'], ['0', '2', '0'], ['1', '0.5', '1'] ,['0.9', '2', '1']]
 
     def tearDown(self):
         # delete variables to release memory
@@ -26,13 +43,40 @@ class TestDataset:
 
     def test_setTransformer(self):
         _dataset = Dataset(self.header, self.rows)
-        _transformer = Transformer()
+        _transformer = CustomTransformer()
         _dataset.setTransformer(_transformer)
 
         assert_equals(_dataset.transformer, _transformer)
 
     def test_applyTransformations(self):
-        pass
+        _dataset = Dataset(self.header, self.rows)
+        _transformer = CustomTransformer()
+        _dataset.setTransformer(_transformer)
+
+        _header = ['a1', 'a2', 'f1', 'y']
+        _rows = [[0, 0, 0, '0'], [0, -2, 2, '0'], [-1, -0.5, 1.5, '1'] ,[-0.9, -2, 2.9, '1']]
+
+        _dataset.applyTransformations()
+
+        assert_items_equal(_dataset.header, self.header)
+        assert_items_equal(_dataset.rows, self.rows)
+        assert_equals(_dataset.transformed_header, _header)
+        assert_items_equal(_dataset.transformed_rows, _rows)
+        assert_equals(_dataset.transformer, _transformer)
 
     def test_applyRowTransformation(self):
-        pass
+        _dataset = Dataset(self.header, self.rows)
+        _transformer = CustomTransformer()
+        _dataset.setTransformer(_transformer)
+
+        _row = ['1', '20', '2']
+        _expected_row = [[-1, -20, 21, '2']]
+        _header = ['a1', 'a2', 'f1', 'y']
+
+        _dataset.applyRowTransformation(_row)
+
+        assert_items_equal(_dataset.header, self.header)
+        assert_items_equal(_dataset.rows, self.rows)
+        assert_equals(_dataset.transformed_header, _header)
+        assert_items_equal(_dataset.transformed_rows, _expected_row)
+        assert_equals(_dataset.transformer, _transformer)
