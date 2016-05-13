24e780dd0f30e4bf9696a6fd185d20fb297f0bd0
George Theofilis <theofilis.g@gmail.com>
2016-05-13 17:18:25 +0300

    Add documentation and some methods

--- rsk_mind/transformer/transformer.py
+++ rsk_mind/transformer/transformer.py
@@ -1,5 +1,12 @@
 class Transformer(object):
-    class Feats():
+    """
+        Base class for all transformer
+    """
+
+    class Feats:
+        """
+            Define feats on dataset
+        """
         exclude = None
 
     def __init__(self):
@@ -7,7 +14,23 @@ class Transformer(object):
             getattr(self.Feats, field).bind(field, self)
 
     def get_feats(self):
+        """
+
+        :return: a list of feats
+        """
         return [x for x in dir(self.Feats) if not (x.startswith('__') or x in ['exclude'])]
 
     def get_transformer_func(self, feat_name):
+        """
+
+        :param feat_name: name of feat
+        :return: a transformer function on feat
+        """
         return getattr(self.Feats, feat_name).transform
+
+    def get_excluded_feats(self):
+        """
+
+        :return: a list with excluded feats
+        """
+        return self.Feats.exclude
