b34913de8b1326bf3baafc51e819a382032d4fcb
Konstantinos Kostis <kekostis@gmail.com>
2016-05-26 22:36:53 +0300

    xgboost classifier development

--- rsk_mind/classifier/xgboost_classifier.py
+++ rsk_mind/classifier/xgboost_classifier.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xgboost as xgb
 from classifier import Classifier
+from utils import separate_data_from_class
 
 
 class XgboostClassifier(Classifier):
@@ -17,24 +18,55 @@ class XgboostClassifier(Classifier):
         """
         if not parameters:
             parameters = {
-                'bst:max_depth':7,
+                'bst:max_depth': 7,
                 'bst:eta': 0.3,
-                'bst:subsample':0.5,
-                'silent':1,
-                'objective':'binary:logistic',
-                'nthread':4,
-                'eval_metric':'auc'
+                'bst:subsample': 0.5,
+                'silent': 0,
+                'objective': 'binary:logistic',
+                'nthread': 4,
+                'eval_metric': 'auc'
             }
         number_of_rounds = 200
 
-        dtrain = None
         if not self._training_dataset:
             # raise a specific error later
             raise Exception()
+        else:
+            data_sequence, targets_sequence = separate_data_from_class(
+                    self._training_dataset)
+            data = np.array(data_sequence[1])
+            targets = np.array(targets_sequence[1])
+            train_dmatrix = xgb.DMatrix(data, label=targets)
 
-        eval_list = []
         if self._validation_dataset:
+            vdata_sequence, vtargets_sequence = separate_data_from_class(
+                    self._validation_dataset)
+            vdata = np.array(vdata_sequence[1])
+            vtargets = np.array(vtargets_sequence[1])
+            validation_dmatrix = xgb.DMatrix(vdata, label=vtargets)
+            eval_list = [(validation_dmatrix, 'evaluation'), (train_dmatrix, 'train')]
+        else:
+            eval_list = []
+
+        self.model = xgb.train(parameters, train_dmatrix, number_of_rounds, eval_list)
 
 
+    def save_model(self, path):
+        if self.model:
+            self.model.save_model(path)
+        else:
+            # raise custom error later
+            raise Exception()
 
+    def load_model(self, path):
+        self.model = xgb.Booster({'nthread':4})
+        # wrap bellow statement in a try-except statement
+        # with custom exception
+        self.model.load_model(path)
+
+    def evaluate(self, threshold=0.5):
+        pass
+
+    def predict(instance):
+        pass
 
